2fa32dab7ae884cba672dce41ba91952a889018f
alexey.piskun <alexey.piskun@tronderenergi.no>
2020-07-19 01:28:38 +0200

    updated loader

--- scripts/loader.py
+++ scripts/loader.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv import load_dotenv
+from influxdb import DataFrameClient
+import numpy as np
 import pandas as pd
 from pgcom import Commuter
 
@@ -8,7 +10,7 @@ base_url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/"
 root_dir = os.path.dirname(os.path.abspath(os.path.join(__file__, '..')))
 
 
-def read_data(filename, index=None, rename_col=None):
+def read_data(filename, rename_col=None):
     """Read data from csv.
     """
     df = pd.read_csv(base_url + filename)
@@ -17,17 +19,18 @@ def read_data(filename, index=None, rename_col=None):
         df.rename(columns=rename_col, inplace=True)
     if 'date' in df.columns:
         df['date'] = pd.to_datetime(df['date'])
-    if index is not None:
-        df.set_index(index, inplace=True)
     return df
 
 
-def get_conn_params(db='POSTGRES'):
+def get_conn_params(db='postgres'):
     """Return database connection parameters.
     """
     params = dict()
     for key in ['host', 'port', 'user', 'password', 'dbname']:
-        params[key] = os.getenv(db + '_' + key.upper())
+        params[key] = os.getenv(db.upper() + '_' + key.upper())
+    if db == 'influx':
+        params['username'] = params.pop('user')
+        params['database'] = params.pop('dbname')
     return params
 
 
@@ -36,15 +39,47 @@ if __name__ == '__main__':
     load_dotenv(os.path.join(root_dir, 'docker', '.env'))
 
     # init postgres client
-    conn_params = get_conn_params()
+    conn_params = get_conn_params(db='postgres')
     commuter = Commuter(**conn_params)
+
+    # init influx client
+    conn_params = get_conn_params(db='influx')
+    client = DataFrameClient(**conn_params)
+
+    # read countries reference data
+    rename_col = {'country_region': 'country', 'long_': 'lon'}
+    df = read_data('reference.csv', rename_col=rename_col)
+    commuter.copy_from(
+        table_name='countries_ref',
+        data=df,
+        schema='covid',
+        format_data=True,
+        where='1=1')
 
     # read aggregated statistics by countries
     df = read_data('countries-aggregated.csv')
-    # upload data to postgres
+    # upload to postgres
     commuter.copy_from(
-        'countries_aggregated', df, schema='covid', format_data=True)
+        table_name='countries_aggregated',
+        data=df,
+        schema='covid',
+        format_data=True,
+        where='1=1')
 
-    # read countries reference data
-    df = read_data('reference.csv', rename_col={'long_': 'lon'})
-    commuter.copy_from('countries_ref', df, schema='covid', format_data=True)
+    # upload to influx
+    df.set_index('date', inplace=True)
+    df['active'] = df['confirmed'] - df['recovered']
+    client.write_points(
+        dataframe=df,
+        measurement='active_cases',
+        tag_columns=['country'],
+        field_columns=['active'],
+        protocol='line')
+
+    df['active_scaled'] = np.log(1 + 0.0001 * df['active'])
+    client.write_points(
+        dataframe=df,
+        measurement='active_scaled',
+        tag_columns=['country'],
+        field_columns=['active_scaled'],
+        protocol='line')
