2e13a50f36681294331cb59a61b1881a31f91a58
alex <piskun.aleksey@gmail.com>
2020-08-02 14:02:49 +0200

    fixed sitemap

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,5 +45,3 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-
-html_extra_path = ['robots.txt', 'sitemap.xml']
